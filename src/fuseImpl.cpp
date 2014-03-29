210b2785ee3eadedd49a664aeb832b0d6058e8b0
matteo <matteo@portatile.casa>
2014-03-29 11:55:57 +0100

    cleaning

--- src/fuseImpl.cpp
+++ src/fuseImpl.cpp
@@ -15,23 +15,23 @@ static MegaFuse* megaFuse;
 
 
 
-extern "C" int hello_write(const char * path, const char *buf, size_t size, off_t offset, struct fuse_file_info * fi)
+int hello_write(const char * path, const char *buf, size_t size, off_t offset, struct fuse_file_info * fi)
 {
 
     return megaFuse->write(path,buf,size,offset,fi);
 }
 
-extern "C" int hello_getattr(const char *path, struct stat *stbuf)
+int hello_getattr(const char *path, struct stat *stbuf)
 {
 	return megaFuse->getAttr(path,stbuf);
 }
 
-extern "C" int hello_open(const char *path, struct fuse_file_info *fi)
+int hello_open(const char *path, struct fuse_file_info *fi)
 {
     return megaFuse->open(path,fi);
 }
 
-extern "C" int hello_read(const char *path, char *buf, size_t size, off_t offset,
+int hello_read(const char *path, char *buf, size_t size, off_t offset,
 		      struct fuse_file_info *fi)
 {
 	//int s = pread(fi->fh,buf,size,offset);
@@ -39,33 +39,33 @@ extern "C" int hello_read(const char *path, char *buf, size_t size, off_t offset
 	return megaFuse->read(path,buf,size,offset,fi);
 }
 
-extern "C" int hello_rename(const char * src, const char *dst)
+int hello_rename(const char * src, const char *dst)
 {
     return megaFuse->rename(src,dst);
 }
-extern "C" int hello_unlink(const char *path)
+int hello_unlink(const char *path)
 {
     return megaFuse->unlink(path);
 }
-extern "C" int hello_release(const char *path, struct fuse_file_info *fi)
+int hello_release(const char *path, struct fuse_file_info *fi)
 {
     return megaFuse->release(path,fi);
 }
 
 
-extern "C" int hello_mkdir(const char * path, mode_t mode)
+int hello_mkdir(const char * path, mode_t mode)
 {
     return megaFuse->mkdir(path,mode);
 
 }
-extern "C" int hello_create(const char *path, mode_t mode, struct fuse_file_info * fi)
+int hello_create(const char *path, mode_t mode, struct fuse_file_info * fi)
 {
     return megaFuse->create(path,mode,fi);
 }
 
 
 
-extern "C" int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 			 off_t offset, struct fuse_file_info *fi)
 {
 	return megaFuse->readdir(path,buf,filler,offset,fi);
@@ -93,16 +93,7 @@ int hello_statvfs(const char * a, struct statvfs * stat)
 	
 }
 
-extern "C" int megafuse_main(int argc,char**argv);
 
-
-
-
-int megafuse_main(int argc,char**argv)
-{
-    return 0;
-
-}
 static struct fuse_operations hello_oper;
 
 int megafuse_mainpp(int argc,char**argv,MegaFuse* mf)
